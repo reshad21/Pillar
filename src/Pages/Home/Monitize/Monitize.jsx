55d4392a54c93c0fba9f4a986cbf34731b7861cc
reshad21 <miakala7@gmail.com>
2022-12-16 16:15:25 +0600

    second section work start

---
+++ src/Pages/Home/Monitize/Monitize.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Monitize = () => {
+    return (
+        <div>
+            <h1>This is monitizw</h1>
+        </div>
+    );
+};
+
+export default Monitize;
--- src/Routes/route.jsx
+++ src/Routes/route.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import Home from '../Pages/Home/Home';
+import Home from '../Pages/Home/Home/Home';
 import Main from './../Layout/Main';
 
 const router = createBrowserRouter([
