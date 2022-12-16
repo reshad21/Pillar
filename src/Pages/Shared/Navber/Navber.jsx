af6c22e261565facfce31b966a8d061d55a24086
reshad21 <miakala7@gmail.com>
2022-12-16 10:36:00 +0600

    navber design successfully

--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
 const Navber = () => {
+    const menuItems = <>
+        <li><a href='/' className='font-semibold'>Home</a></li>
+        <li><a href='/' className='font-semibold'>Brands</a></li>
+        <li><a href='/' className='font-semibold'>Agencies</a></li>
+        <li><a href='/' className='font-semibold'>Blog</a></li>
+        <li><a href='/' className='text-primary font-bold'>Login</a></li>
+        <li><a href='/' className='bg-indigo-900 text-slate-50'>Sign Up</a></li>
+    </>
     return (
         <div>
-            <h1>This is navber</h1>
+            <div className="navbar bg-base-100 justify-between py-16">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <a href='/' className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {menuItems}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
